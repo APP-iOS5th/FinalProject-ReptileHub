29bcad7378e1bd19d9ca9b01320710d96b9659a9
rmfls0606 <rmfls0606@naver.com>
2024-08-08 10:03:29 +0900

    [Chore] 편의를 위한 uikit 미리보기 기능 추가

--- ReptileHub/Controller/GrowthDiary/GrowthDiaryViewController.swift
+++ ReptileHub/Controller/GrowthDiary/GrowthDiaryViewController.swift
@@ -27,3 +27,28 @@ class GrowthDiaryViewController: UIViewController {
      */
     
 }
+
+#if DEBUG
+import SwiftUI
+struct ViewControllerRepresentable: UIViewControllerRepresentable {
+    
+    func updateUIViewController(_ uiView: UIViewController,context: Context) {
+        // leave this empty
+    }
+    @available(iOS 13.0.0, *)
+    func makeUIViewController(context: Context) -> UIViewController{
+        GrowthDiaryViewController()
+    }
+}
+@available(iOS 13.0, *)
+struct ViewControllerRepresentable_PreviewProvider: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ViewControllerRepresentable()
+                .ignoresSafeArea()
+                .previewDisplayName(/*@START_MENU_TOKEN@*/"Preview"/*@END_MENU_TOKEN@*/)
+                .previewDevice(PreviewDevice(rawValue: "iPhone 15 Pro"))
+        }
+        
+    }
+} #endif
