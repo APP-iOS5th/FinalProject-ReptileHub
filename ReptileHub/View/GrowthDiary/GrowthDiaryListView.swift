5abc73209cb09250e8803c07160a29c1e2c25af5
rmfls0606 <rmfls0606@naver.com>
2024-08-13 00:49:13 +0900

    [Chore] 편의를 위한 UIView preview 기능 추가

--- ReptileHub/View/GrowthDiary/GrowthDiaryListView.swift
+++ ReptileHub/View/GrowthDiary/GrowthDiaryListView.swift
@@ -10,11 +10,34 @@ import UIKit
 class GrowthDiaryListView: UIView {
 
     /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
+     Only override draw() if you perform custom drawing.
+     An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
-        // Drawing code
+         Drawing code
     }
     */
-
+    
 }
+
+#if DEBUG
+import SwiftUI
+
+struct GrowthDiaryListViewRepresentable: UIViewRepresentable {
+    func makeUIView(context: Context) -> GrowthDiaryListView {
+        return GrowthDiaryListView()
+    }
+    
+    func updateUIView(_ uiView: GrowthDiaryListView, context: Context) {
+        // 필요하다면 뷰 업데이트
+        uiView.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        uiView.setContentHuggingPriority(.defaultHigh, for: .vertical)
+    }
+}
+
+struct MyCustomView_Previews: PreviewProvider {
+    static var previews: some View {
+        GrowthDiaryListViewRepresentable()
+            .previewLayout(.sizeThatFits) // 크기를 맞춤 설정할 수 있음
+    }
+}
+#endif
