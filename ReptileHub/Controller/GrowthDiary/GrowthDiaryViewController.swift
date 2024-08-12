1179e77276a45a188c53b946476e1388d3066d43
rmfls0606 <rmfls0606@naver.com>
2024-08-12 13:19:31 +0900

    [Feat] 성장 일지 목록 뷰 파일 생성 및 폴더 구조 변경

--- ReptileHub/Controller/GrowthDiary/GrowthDiaryViewController.swift
+++ ReptileHub/Controller/GrowthDiary/GrowthDiaryViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ReportViewController.swift
+//  GrowthDiaryViewController.swift
 //  ReptileHub
 //
 //  Created by 조성빈 on 8/5/24.
---
+++ ReptileHub/View/GrowthDiary/GrowthDiaryListView.swift
@@ -0,0 +1,20 @@
+//
+//  GrowthDiaryListView.swift
+//  ReptileHub
+//
+//  Created by 이상민 on 8/12/24.
+//
+
+import UIKit
+
+class GrowthDiaryListView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
