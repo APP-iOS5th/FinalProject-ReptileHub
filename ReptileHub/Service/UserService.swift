5018861bdba277b83d7dfed49ade6f052c47c680
LimJaeHyeon9298 <limjh9298@gmail.com>
2024-08-17 14:24:58 +0900

    [feat] 유저별 작성한 게시글 썸네일 정보 불러오는 함수 구현

--- ReptileHub/Service/UserService.swift
+++ ReptileHub/Service/UserService.swift
@@ -80,5 +80,50 @@ class UserService {
         }
         
     }
+    //MARK: - 해당 유저 게시글 정보 필터링 해서 가져올 수 있는 함수
+    func fetchUserPostsThumbnails(userID: String, completion: @escaping (Result<[ThumbnailPostResponse], Error>) -> Void) {
+        let db = Firestore.firestore()
+        
+        // posts 컬렉션에서 특정 userID를 가진 문서만 가져옴(내 정보 가져올시 내 uid 사용)
+        db.collection("posts")
+            .whereField("userID", isEqualTo: userID)
+            .getDocuments { (querySnapshot, error) in
+                if let error = error {
+                    completion(.failure(error))
+                    return
+                }
+                
+                var thumbnails: [ThumbnailPostResponse] = []
+                
+                // 각 문서의 썸네일 정보를 수집
+                for document in querySnapshot?.documents ?? [] {
+                    let data = document.data()
+                    
+                    if let postID = data["postID"] as? String,
+                       let title = data["title"] as? String,
+                       let thumbnailURL = data["thumbnail"] as? String,
+                       let previewContent = data["previewContent"] as? String,
+                       let likeCount = data["likeCount"] as? Int,
+                       let commentCount = data["commentCount"] as? Int,
+                       let createdAt = data["createdAt"] as? Timestamp {
+                        
+                        let thumbnailResponse = ThumbnailPostResponse(
+                            postID: postID,
+                            title: title,
+                            userID: userID,
+                            thumbnailURL: thumbnailURL,
+                            previewContent: previewContent,
+                            likeCount: likeCount,
+                            commentCount: commentCount,
+                            createdAt: createdAt.dateValue()
+                        )
+                        thumbnails.append(thumbnailResponse)
+                    }
+                }
+                
+                completion(.success(thumbnails))
+            }
+    }
+    
     
 }
