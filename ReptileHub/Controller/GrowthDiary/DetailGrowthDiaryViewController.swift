f4ac4386b1a47ae64321f10352e007066a3912ef
rmfls0606 <rmfls0606@naver.com>
2024-09-01 23:06:27 +0900

    [Feat] 성장일지 상세 뷰 파일 생성

---
+++ ReptileHub/Controller/GrowthDiary/DetailGrowthDiaryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailGrowthDiaryViewController.swift
+//  ReptileHub
+//
+//  Created by 이상민 on 9/1/24.
+//
+
+import UIKit
+
+class DetailGrowthDiaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.title = "반려 도마뱀 프로필"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ReptileHub/View/GrowthDiary/DetailGrowthDiaryView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailGrowthDiaryView.swift
+//  ReptileHub
+//
+//  Created by 이상민 on 9/1/24.
+//
+
+import UIKit
+
+class DetailGrowthDiaryView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
