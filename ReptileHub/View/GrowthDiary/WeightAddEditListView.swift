069980d79c51aad8d623386652832ee2476fbf1e
rmfls0606 <rmfls0606@naver.com>
2024-09-07 22:07:40 +0900

    [feat] 무게 추이 목록 뷰(연도, 월, 일)별로 표현하기 작성 중

--- ReptileHub/View/GrowthDiary/WeightAddEditListView.swift
+++ ReptileHub/View/GrowthDiary/WeightAddEditListView.swift
@@ -9,9 +9,43 @@ import UIKit
 
 class WeightAddEditListView: UIView {
     
+    //MARK: - 카테고리 버튼 클로저
+    private lazy var categoryButton = { (text: String) -> UIButton in
+        let button = UIButton()
+        var config = UIButton.Configuration.plain()
+        config.title = text
+        config.baseBackgroundColor = .red
+        button.configuration = config
+        return button
+    }
+    
+    //MARK: - 연 버튼
+    private lazy var yearCategory = categoryButton("연도")
+    //MARK: - 월 버튼
+    private lazy var monthCategory = categoryButton("월")
+    //MARK: - 일 벝튼
+    private lazy var dayCategory = categoryButton("일")
+    
+    //MARK: - 카테고리 스택 뷰
+    private lazy var categoryStackView: UIStackView = {
+        let view = UIStackView(arrangedSubviews: [yearCategory, monthCategory, dayCategory])
+        view.axis = .horizontal
+        view.spacing = 10
+        view.distribution = .fillEqually
+        return view
+    }()
+    
     //MARK: - 무게 추이 테이블 뷰
     private lazy var weightAddEditTableView: UITableView = {
         let view = UITableView()
+        return view
+    }()
+    
+    private lazy var mainView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .yellow
+        view.addSubview(categoryStackView)
+        view.addSubview(weightAddEditTableView)
         return view
     }()
     
@@ -24,10 +58,19 @@ class WeightAddEditListView: UIView {
     }
     
     func setUI(){
-        self.addSubview(weightAddEditTableView)
+        self.addSubview(mainView)
+        
+        mainView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        categoryStackView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalTo(mainView)
+        }
         
         weightAddEditTableView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
+            make.top.equalTo(categoryStackView.snp.bottom)
+            make.leading.trailing.bottom.equalToSuperview()
         }
     }
     
